16d15f2ebc69a15dc351e5ba57c174c5fe98ea27
sbarex <sbarex@gmail.com>
2026-02-10 11:28:06 +0100

    Fix missing argument documentation.

--- include/apex/apex.h
+++ include/apex/apex.h
@@ -248,6 +248,8 @@ char *apex_markdown_to_html(const char *markdown, size_t len, const apex_options
  * @param content HTML content to wrap
  * @param title Document title (NULL for default)
  * @param stylesheet_paths NULL-terminated array of CSS file paths to link in head
+ * @param stylesheet_count Number of CSS files on the stylesheet_paths array.
+ * @param code_highlighter Highlighter engine.
  * @param html_header Raw HTML to insert in <head> section (NULL for none)
  * @param html_footer Raw HTML to append before </body> (NULL for none)
  * @param language Language code for <html lang> attribute (NULL for "en")
